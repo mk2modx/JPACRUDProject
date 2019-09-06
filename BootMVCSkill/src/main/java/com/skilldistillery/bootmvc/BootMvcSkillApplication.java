123e6032f78ccce9f1d7f64bac5f301dc2348178
Mark Ag <mk2modx@icloud.com>
2019-09-06 16:32:48 -0600

    Setting up Boot

---
+++ BootMVCSkill/src/main/java/com/skilldistillery/bootmvc/BootMvcSkillApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcSkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcSkillApplication.class, args);
+	}
+
+}
